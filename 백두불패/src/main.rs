bac907737371854ceee8029a9f398857b6628ca1
Duswnsey <duswnsey@gmail.com>
2026-04-13 15:59:22 -0400

    백두불패 - 구문분석기 갱신

--- 백두불패/src/main.rs
+++ 백두불패/src/main.rs
@@ -45,7 +45,7 @@ fn parser(token_tree: &Vec<Token>, index: &mut usize, top: bool) -> Vec<Element>
                         panic!();
                     }
                     *index += 1;
-                    let typee = get_type(&token_tree, index, 0);
+                    let typee = get_pattern(&token_tree, index);
                     arguments.push((name.clone(), typee));
                     if Some(&Token::GoalHoClose) == token_tree.get(*index) {
                         *index += 1;
@@ -57,7 +57,7 @@ fn parser(token_tree: &Vec<Token>, index: &mut usize, top: bool) -> Vec<Element>
                 }
                 let return_type = if token_tree.get(*index) == Some(&Token::ReturnType) {
                     *index += 1;
-                    Some(get_type(&token_tree, index, 0))
+                    Some(get_pattern(&token_tree, index))
                 } else {
                     None
                 };
@@ -82,7 +82,7 @@ fn parser(token_tree: &Vec<Token>, index: &mut usize, top: bool) -> Vec<Element>
             }
             _ => {
                 *index -= 1;
-                let value = get_type(token_tree, index, 0);
+                let value = get_expression(token_tree, index, 0, true);
                 if token_tree.get(*index) == Some(&Token::SemiColumn) {
                     *index += 1;
                     result.push(Element::Thatsit(value));
@@ -97,31 +97,36 @@ fn parser(token_tree: &Vec<Token>, index: &mut usize, top: bool) -> Vec<Element>
     }
     result
 }
-fn get_type(tree: &Vec<Token>, index: &mut usize, offset: usize) -> Type {
+fn get_pattern(tree: &[Token], index: &mut usize) -> Pattern {
+    Pattern::NotImplementedPattern
+}
+fn get_expression(tree: &Vec<Token>, index: &mut usize, offset: usize, full: bool) -> Expression {
     let mut result;
     match tree.get(*index + offset) {
         Some(Token::KeyWord(name)) => {
             *index += 1;
             if tree.get(*index) == Some(&Token::GoalHoOpen) {
-                let arguments = if let Type::Tuple(args) = get_type(tree, index, 0) {
-                    args.clone()
-                } else {
-                    panic!()
-                };
-                result = Type::Function(name.to_string(), arguments);
+                
+                let arguments: Vec<Expression> =
+                    if let Expression::Tuple(tup) = get_expression(tree, index, 0, false) {
+                        tup.clone()
+                    } else {
+                        panic!();
+                    };
+                result = Expression::Function(name.to_string(), arguments);
             } else {
-                result = Type::Other(name.to_string());
+                result = Expression::Other(name.to_string());
             }
         }
         Some(Token::GoalHoOpen) => {
             *index += 1;
-            result = Type::Tuple(Vec::new());
+            result = Expression::Tuple(Vec::new());
             if tree.get(*index) == Some(&Token::GoalHoClose) {
                 *index += 1;
             } else {
                 loop {
-                    if let Type::Tuple(ref mut tup) = result {
-                        tup.push(get_type(tree, index, 0));
+                    if let Expression::Tuple(ref mut tup) = result {
+                        tup.push(get_expression(tree, index, 0, true));
                     }
                     match tree.get(*index + offset) {
                         Some(Token::GoalHoClose) => {
@@ -141,43 +146,126 @@ fn get_type(tree: &Vec<Token>, index: &mut usize, offset: usize) -> Type {
         }
         Some(Token::Str(string)) => {
             *index += 1;
-            result = Type::Str(string.clone());
+            result = Expression::Str(string.clone());
         }
         _ => {
             panic!()
         }
     }
-    if tree.get(*index) == Some(&Token::Dot) {
-        *index += 1;
-        result = Type::Chain(Box::new(result), Box::new(get_type(tree, index,0)));
+    if full {
+        loop {
+            match tree.get(*index) {
+                Some(&Token::Dot) => {
+                    *index += 1;
+                    result = Expression::Chain(
+                        Box::new(result),
+                        Box::new(get_expression(tree, index, 0, false)),
+                    );
+                }
+                Some(&Token::Add) => {
+                    *index += 1;
+                    expression_calculate(&mut result, CalculationType::Add, index, tree);
+                }
+                Some(&Token::Multifly) => {
+                    *index += 1;
+                    expression_calculate(&mut result, CalculationType::Multiply, index, tree);
+                }
+                Some(&Token::Square) => {
+                    *index += 1;
+                    expression_calculate(&mut result, CalculationType::Square, index, tree);
+                }
+                Some(&Token::Equal) => {
+                    *index += 1;
+                    expression_calculate(&mut result, CalculationType::Equal, index, tree);
+                }
+                _ => break,
+            }
+        }
     }
     result
+}
+//사칙련산 관련 처리
+fn expression_calculate(
+    current: &mut Expression,
+    calculation_type: CalculationType,
+    index: &mut usize,
+    tree: &Vec<Token>,
+) {
+    //앞 표현식이 련산 고려 대상이 아니라면
+    if !matches!(current, Expression::Calculation(_, _, _)) {
+        *current = Expression::Calculation(
+            calculation_type,
+            Box::new(current.clone()),
+            Box::new(get_expression(tree, index, 0, false)),
+        );
+        return;
+    }
+    let cloned = current.clone();
+    if let Expression::Calculation(current_calc, _, s) = current {
+        if current_calc.get_priority() < calculation_type.get_priority() {
+            expression_calculate(&mut *s, calculation_type.clone(), index, tree);
+        } else {
+            *current = Expression::Calculation(
+                calculation_type.clone(),
+                Box::new(cloned),
+                Box::new(get_expression(tree, index, 0, false)),
+            )
+        }
+    }
+}
+impl CalculationType {
+    fn get_priority(&self) -> usize {
+        match self {
+            CalculationType::Equal => 0,
+            CalculationType::Add => 1,
+            CalculationType::Multiply => 2,
+            CalculationType::Square => 3,
+        }
+    }
+}
+#[derive(Debug, Clone)]
+enum CalculationType {
+    Add,
+    Multiply,
+    Square,
+    Equal,
+}
+#[derive(Debug)]
+enum Pattern {
+    NotImplementedPattern,
 }
 type Ast = Vec<Element>;
 #[derive(Debug)]
 enum Element {
+    //statement
     Function {
-        argument: Vec<(String, Type)>,
+        argument: Vec<(String, Pattern)>,
         name: String,
         code: Ast,
-        return_type: Option<Type>,
+        return_type: Option<Pattern>,
     },
-    Expose(Type),
-    Thatsit(Type),
+    Expose(Expression),
+    Thatsit(Expression),
 }
 #[derive(Debug, Clone)]
-enum Type {
-    Tuple(Vec<Type>),
+enum Expression {
+    Tuple(Vec<Expression>),
     Other(String),
-    Function(String, Vec<Type>),
+    Function(String, Vec<Expression>),
     Str(String),
-    Chain(Box<Type>, Box<Type>),
+    Chain(Box<Expression>, Box<Expression>),
+    // 사슬 함수로 변환할 예정
+    // 사슬함수에서 연산자를 호출하는것도 사칙 련산이 적용되기 때문 */
+    Calculation(CalculationType, Box<Expression>, Box<Expression>),
 }
+
 fn lexer(raw: String) -> Vec<Token> {
     let iter: Vec<char> = raw.chars().collect();
     let mut index = 0;
     let mut result = Vec::new();
+    //문자 핵심어를 임시적으로 수용함
     let mut keyword = String::new();
+    //기호 핵심어를 처리함.
     loop {
         if peek(&iter, &mut index, "<") {
             flush_keyword(&mut keyword, &mut result);
@@ -219,6 +307,18 @@ fn lexer(raw: String) -> Vec<Token> {
         } else if peek(&iter, &mut index, ",") {
             flush_keyword(&mut keyword, &mut result);
             result.push(Token::Comma);
+        } else if peek(&iter, &mut index, "+") {
+            flush_keyword(&mut keyword, &mut result);
+            result.push(Token::Add);
+        } else if peek(&iter, &mut index, "*") {
+            flush_keyword(&mut keyword, &mut result);
+            result.push(Token::Multifly);
+        } else if peek(&iter, &mut index, "^") {
+            flush_keyword(&mut keyword, &mut result);
+            result.push(Token::Square);
+        } else if peek(&iter, &mut index, "==") {
+            flush_keyword(&mut keyword, &mut result);
+            result.push(Token::Equal);
         } else if peek(&iter, &mut index, "\"") {
             let mut string = String::new();
             loop {
@@ -237,10 +337,9 @@ fn lexer(raw: String) -> Vec<Token> {
             result.push(Token::Str(string));
             continue;
         } else if peek(&iter, &mut index, ".") {
-  flush_keyword(&mut keyword, &mut result);
-   result.push(Token::Dot);
-         }
-           else if let Some(ch) = iter.get(index) {
+            flush_keyword(&mut keyword, &mut result);
+            result.push(Token::Dot);
+        } else if let Some(ch) = iter.get(index) {
             keyword.push(*ch);
             index += 1;
         } else {
@@ -248,6 +347,7 @@ fn lexer(raw: String) -> Vec<Token> {
         }
     }
 }
+//문자열 핵심어를 처리
 fn flush_keyword(buffer: &mut String, result: &mut Vec<Token>) {
     if !buffer.is_empty() {
         match buffer.as_str() {
@@ -353,10 +453,15 @@ enum Token {
     Str(String),
     Ignore(char),
     Comma,
-    Dot
+    Dot,
+    Multifly,
+    Square,
+    Add,
+    Equal,
 }
-fn peek(array: &Vec<char>, index: &mut usize, str: &str) -> bool {
-    let index_failback = index.clone();
+//문자열 엿보는 도우미 함수마당
+fn peek(array: &[char], index: &mut usize, str: &str) -> bool {
+    let index_failback = *index;
     for i in str.chars() {
         if array.get(*index) == Some(&i) {
             *index += 1;
